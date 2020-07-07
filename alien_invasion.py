75412fd010771ce07b03b51fa1afa74216047acf
Jason <405339556@qq.com>
2020-07-07 22:49:54 +0800

    更新外星人记分功能

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,8 @@ from pygame.sprite import Group
 
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
+from button import Button
 from ship import Ship
 import game_functions as gf
 
@@ -15,8 +17,14 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
 
+    # 创建Play按钮
+    play_button = Button(ai_settings, screen, "Play")
+
     # 创建一个用于存储游戏统计信息的实例
     stats = GameStats(ai_settings)
+
+    # 创建存储游戏统计信息的实例，并创建记分牌
+    sb = Scoreboard(ai_settings, screen, stats)
 
     # 创建一艘飞船，一个子弹编组和一个外星人编组
     ship = Ship(ai_settings, screen)
@@ -30,15 +38,15 @@ def run_game():
     # 开始游戏的主循环
     while True:
         # 监视键盘和鼠标事件
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
 
         if stats.game_active:
             ship.update()
-            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
 
         # 更新屏幕上的图像，并切换到新屏幕
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
 
 
 run_game()
